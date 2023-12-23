60a13c6dcc59c139b9105ebab49fed9a1f74dd5e
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-12-23 16:21:28 -0300

    Add model for Ingredient

--- culinary_registry/models.py
+++ culinary_registry/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=120)
+    brand = models.CharField(max_length=120, blank=True)
+    amount = models.FloatField()
+    carbohydrate = models.FloatField()
+    protein = models.FloatField()
+    total_fat = models.FloatField()
+    trans_fat = models.FloatField()
+    saturated_fat = models.FloatField()
+    fiber = models.FloatField()
+    sodium = models.FloatField()
+    calory = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
