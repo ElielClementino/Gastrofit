3b63d38d978615c6dae5be92b7de185bacf0e9ea
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-12-23 17:40:19 -0300

    Improve admin panel visualization

--- culinary_registry/admin.py
+++ culinary_registry/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from culinary_registry.models import Ingredient
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'brand',)
+    ordering = ('name',)
