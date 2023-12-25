3b49d46d156743fc635b5560d62c216ea2621e51
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-12-25 14:29:19 -0300

    Add expected test for ingredient registration

--- culinary_registry/tests/test_culinary_registry.py
+++ culinary_registry/tests/test_culinary_registry.py
@@ -1,6 +1,7 @@
 from model_bakery import baker
 from culinary_registry.models import Recipe, RecipeIngredient, Ingredient
 
+
 def calculate_calories(obj):
      """
      Logic being used on model to calculate automatically the calories of an ingredient, when
@@ -16,7 +17,7 @@ def calculate_calories(obj):
         
         return calories
 
-def test_if_calories_is_being_calculated_correctly(client, db):
+def test_if_calories_is_being_calculated_correctly(db):
     Ingredient.objects.create(
         name = "Farinha de Trigo",
         brand = "Marca",
@@ -35,3 +36,29 @@ def test_if_calories_is_being_calculated_correctly(client, db):
     calories = calculate_calories(ingredient)
 
     assert ingredient.calories == calories
+
+
+def test_add_new_ingredient(client, db):
+
+    new_ingredient = {
+        "name": "Farinha de Trigo",
+        "brand": "Marca",
+        "amount": 100,
+        "carbohydrate": 15.02,
+        "protein": 1.96,
+        "total_fat": 0.28,
+        "trans_fat": 0,
+        "saturated_fat": 0.04,
+        "fiber": 0.46,
+        "sodium": 0.20,
+        "calory": 0,
+    }
+    request = client.post("api/culinary/new/ingredient", new_ingredient, content_type="application/json")
+
+    response = request.json()
+
+    assert request.status_code = 201
+    assert new_ingredient["name"] == response.name
+    assert new_ingredient["brand"] == response.brand
+    assert new_ingredient["amount"] == response.amount
+    assert new_ingredient["calory"] != response.calory
